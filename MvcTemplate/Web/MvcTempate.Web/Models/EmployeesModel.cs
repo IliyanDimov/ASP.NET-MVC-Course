5d117d68d2468875090dc31a1232b4f788848699
Ich0 <iliyan.dimov@gmail.com>
2017-01-31 16:02:00 +0200

    Added EmployeesModel, Started organizing main page

---
+++ MvcTemplate/Web/MvcTempate.Web/Models/EmployeesModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MvcTempate.Web.Models
+{
+    public class EmployeesModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Position { get; set; }
+        public int Salary { get; set; }
+    }
+}
